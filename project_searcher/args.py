3924cdb6997ceb47339c29df4298456d1dbb7485
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-10 14:34:28 -0500

    Update args and delete old code

--- project_searcher/args.py
+++ project_searcher/args.py
@@ -1,133 +1,123 @@
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, HelpFormatter, Namespace
 from datetime import datetime
+from operator import attrgetter
 
-name: str = "PRIME"
-authors: list = [
+
+class SortingHelpFormatter(HelpFormatter):
+    def add_arguments(self, actions):
+        actions = sorted(actions, key=attrgetter("option_strings"))
+        super(SortingHelpFormatter, self).add_arguments(actions)
+
+
+AUTHORS: list = [
     "Nicholas M. Synovic",
     "Matthew Hyatt",
     "George K. Thiruvathukal",
 ]
 
+MAX_VALUE: int = 1000000000
+MIN_VALUE: int = 0
+
 
 def mainArgs() -> Namespace:
     parser: ArgumentParser = ArgumentParser(
-        prog=f"{name} GitHub Repository Searcher",
-        description="A utility to perform advanced searching on GitHub using both the REST and GraphQL APIs",
-        epilog=f"Author(s): {', '.join(authors)}",
+        prog=f"Software Project Searcher",
+        description="Command line tool for searching for software projects on code hosting platforms",
+        epilog=f"Author(s): {', '.join(AUTHORS)}",
+        formatter_class=SortingHelpFormatter,
     )
-    parser.add_argument(
-        "-r",
-        "--repository",
-        help="A specific repository to be analyzed. Must be in format OWNER/REPO",
-        type=str,
-        required=False,
-        default=None,
-    )
-    parser.add_argument(
-        "--topic",
-        help="Topic to scrape (up to) the top 1000 repositories from",
-        type=str,
-        required=False,
-        default=None,
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        help="JSON file to dump data to",
-        type=str,
-        required=True,
-    )
+
+    # Required args
     parser.add_argument(
         "-t",
         "--token",
-        help="GitHub personal access token",
+        help="Code hosting platform API token",
         type=str,
         required=True,
     )
     parser.add_argument(
-        "--min-stars",
-        help="Minimum number of stars a repository must have",
+        "-p",
+        "--platform",
+        help="Code hosting platform name",
+        type=str,
+        choices=["github"],
+        default="github",
+    )
+
+    # Repo language, has default value that can limit the search results
+    parser.add_argument(
+        "--language",
+        help="Language of code in a repository",
+        type=str,
+        required=False,
+        default="java",
+    )
+
+    # Repo size
+    parser.add_argument(
+        "--min-size",
+        help="Minimum size of repository in kilobytes",
         type=int,
         required=False,
-        default=0,
+        default=MIN_VALUE,
     )
     parser.add_argument(
-        "--max-stars",
-        help="Maximum number of stars a repository must have",
+        "--max-size",
+        help="Maximum size of repository in MB",
         type=int,
         required=False,
-        default=1000000000,
+        default=MAX_VALUE,
+    )
+
+    # Repo followers
+    parser.add_argument(
+        "--min-followers",
+        help="Minimum number of followers a repository must have",
+        type=int,
+        required=False,
+        default=MIN_VALUE,
     )
     parser.add_argument(
-        "--min-commits",
-        help="Minimum number of commits a repository must have",
+        "--max-followers",
+        help="Maximum number of followers a repository must have",
         type=int,
         required=False,
-        default=0,
+        default=MAX_VALUE,
     )
-    parser.add_argument(
-        "--max-commits",
-        help="Maximum number of commits a repository must have",
-        type=int,
-        required=False,
-        default=1000000000,
-    )
-    parser.add_argument(
-        "--min-issues",
-        help="Minimum number of issues a repository must have",
-        type=int,
-        required=False,
-        default=0,
-    )
-    parser.add_argument(
-        "--max-issues",
-        help="Maximum number of issues a repository must have",
-        type=int,
-        required=False,
-        default=1000000000,
-    )
-    parser.add_argument(
-        "--min-pull-requests",
-        help="Minimum number of pull requests a repository must have",
-        type=int,
-        required=False,
-        default=0,
-    )
-    parser.add_argument(
-        "--max-pull-requests",
-        help="Maximum number of pull requests a repository must have",
-        type=int,
-        required=False,
-        default=1000000000,
-    )
+
+    # Repo forks
     parser.add_argument(
         "--min-forks",
         help="Minimum number of forks a repository must have",
         type=int,
         required=False,
-        default=0,
+        default=MIN_VALUE,
     )
     parser.add_argument(
         "--max-forks",
         help="Maximum number of forks a repository must have",
         type=int,
         required=False,
-        default=1000000000,
+        default=MAX_VALUE,
     )
+
+    # Repo stars
     parser.add_argument(
-        "--min-watchers",
-        help="Minimum number of watchers a repository must have",
+        "--min-stars",
+        help="Minimum number of stars a repository must have",
         type=int,
         required=False,
-        default=0,
+        default=MIN_VALUE,
     )
     parser.add_argument(
-        "--max-watchers",
-        help="Maximum number of watchers a repository must have",
+        "--max-stars",
+        help="Maximum number of stars a repository must have",
         type=int,
         required=False,
-        default=1000000000,
+        default=MAX_VALUE,
     )
+
+    # Repo creation/ updated
     parser.add_argument(
         "--min-created-date",
         help="Minimum date of creation a repository must have",
@@ -156,4 +146,5 @@ def mainArgs() -> Namespace:
         required=False,
         default=datetime.now().strftime("%Y-%m-%d"),
     )
+
     return parser.parse_args()
